f2635e646715aa63fd758a1f971ac2d649a231df
Sotnikov <bars141996@mail.ru>
2015-09-13 21:08:39 +0300

    made a change

--- src/main/java/edu/spbu/cs/IntSort.java
+++ src/main/java/edu/spbu/cs/IntSort.java
@@ -3,13 +3,24 @@ package edu.spbu.cs;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-
+//test commit
 /**
  * Created by artemaliev on 07/09/15.
  */
 public class IntSort {
   public static void sort (int array[]) {
-    Arrays.sort(array);
+    //Arrays.sort(array);
+	  for (int i = 0; i < array.length - 1; i++)
+		{
+		    int j = i + 1;
+			while ((j > 0) && (array[j] < array[j - 1]))
+			{
+	            int temp = array.array[j - 1];
+	            array[j - 1] = array[j];
+	            array[j] = temp;
+	            j--;
+			}
+		}
   }
 
   public static void sort (List<Integer> list) {
